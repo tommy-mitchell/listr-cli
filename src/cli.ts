d332d03cce0c0a7c6bddba8027295d17b5f78dab
tommy-mitchell <tmitchell7@uh.edu>
2023-07-18 00:01:31 -0500

    re-add changes

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,8 @@
-#!/usr/bin/env node
+#!/usr/bin/env tsx
 import process from "node:process";
-import {isCI} from "ci-info";
 import meow from "meow";
-import {Listr} from "listr2";
-import {$} from "execa";
+import { $ } from "execa";
+import { getTasks } from "./tasks.js";
 
 const cli = meow(`
 	Usage
@@ -42,105 +41,30 @@ const cli = meow(`
 	},
 });
 
-const {input: commands} = cli;
-const {help: helpShortFlag, allOptional, hideTimer} = cli.flags;
+const { input: commands, flags: { help: helpShortFlag } } = cli;
 
 if(commands.length === 0 || helpShortFlag) {
 	cli.showHelp(0);
 }
 
-/**
- * Returns the name of a given command and optionally any of its arguments.
- *
- * @param {string} command The given command to parse.
- * @param {object} options Whether or not to return the command's arguments.
- * @param {boolean} options.getArgs Whether or not to return the command's arguments.
- * @returns {string | [commandName: string, args: string[]]}
- */
-const parseCommand = (command, {getArgs = false} = {}) => {
-	const [commandName, ...args] = command.split(" ");
+const { allOptional, hideTimer } = cli.flags;
 
-	return getArgs ? [commandName, args] : commandName;
-};
-
-// Stops task list if `cli.flags.allOptional` is true (default behavior)
-
-/**
- * Fails a task. Stops the task list if `cli.flags.allOptional`
- * is true (default behavior).
- *
- * @param {string} [output] Command output to display, if any.
- */
-const endTask = (output) => {
-	throw new Error(output);
-};
-
-/**
- * Trims command output if it only contains one non-empty line.
- *
- * @param {string} output Command output.
- * @returns {string} The trimmed output, if needed, or the original output.
- */
-const trimIfNeeded = (output) => {
-	const trimmed = output.trim();
-
-	return trimmed.includes("\n") ? output : trimmed;
-};
-
-const tasks = new Listr(commands.map(command => /** @type {import('listr2').ListrTask} */({
-	title: parseCommand(command),
-	task: async ({$$}, task) => {
-		if(isCI) {
-			return $({shell: true, stdio: "inherit"})`${command}`;
-		}
-
-		const [commandName, args] = parseCommand(command, {getArgs: true});
-
-		task.title = `Running "${command}"...`;
-
-		const {exitCode, all, message} = await $$`${commandName} ${args}`;
-
-		if(exitCode === 127 || message?.includes("ENOENT")) {
-			task.title = commandName === command
-				? `${commandName}: command not found.`
-				: `${commandName}: command "${command}" not found.`;
-
-			endTask();
-		}
-
-		task.title = commandName;
-
-		if(exitCode !== 0) {
-			endTask(trimIfNeeded(all));
-		}
-
-		task.output = trimIfNeeded(all);
-	},
-	options: {
-		persistentOutput: true,
-	},
-})), {
+const tasks = getTasks({
+	commands,
 	exitOnError: !allOptional,
-	collectErrors: false,
-	rendererOptions: {
-		showTimer: !hideTimer,
-		collapseErrors: false,
-		formatOutput: "wrap",
-		removeEmptyLines: false,
-	},
-	rendererSilent: isCI,
+	showTimer: !hideTimer,
 });
 
-await tasks.run({
-	$$: $({
-		shell: true,
-		reject: false,
-		all: true,
-		// Keep command output formatting
-		stripFinalNewline: false,
-		env: {
-			// https://github.com/sindresorhus/execa/issues/69#issuecomment-278693026
-			FORCE_COLOR: true,
-		},
-	}),
-}).catch(() => process.exit(1));
+const $$ = $({
+	shell: true,
+	reject: false,
+	all: true,
+	// Keep command output formatting
+	stripFinalNewline: false,
+	env: {
+		// https://github.com/sindresorhus/execa/issues/69#issuecomment-278693026
+		FORCE_COLOR: "true", // eslint-disable-line @typescript-eslint/naming-convention
+	},
+});
+
+await tasks.run({ $$ }).catch(() => process.exit(1));
