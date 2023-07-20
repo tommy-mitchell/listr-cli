f93803547744a6e293121a8cc467b5799a9cd4a5
tommy-mitchell <tmitchell7@uh.edu>
2023-07-20 15:44:40 -0500

    use a regex for `getCommands`

--- src/helpers/get-commands.ts
+++ src/helpers/get-commands.ts
@@ -3,14 +3,41 @@ export type Command = {
 	command: string;
 };
 
-/** Parses CLI input into tasks. Searches for custom names for the task title, defaulting to the first word of a command. */
+type RegExpGroups<T extends string> = (
+	| (RegExpMatchArray & {
+		groups: { [name in T]: string } | Record<string, string>;
+	})
+	| null // eslint-disable-line @typescript-eslint/ban-types
+);
+
+/** @see https://regex101.com/r/zhFo8i/1 */
+const commandRegex = /(?<title>[^:]*):(?<command>.*)/;
+
+/**
+ * Parses CLI input into tasks. Searches for custom names for the task title,
+ * defaulting to the first word of a command.
+ *
+ * @example
+ * ```
+ * getCommands(["lint:xo", "tsd", "coverage and tests:c8 ava"]);
+ *
+ * ```
+ *
+ * ```
+ * [
+ * 	{ taskTitle: "lint",               command: "xo"     },
+ * 	{ taskTitle: "tsd",                command: "tsd"    },
+ * 	{ taskTitle: "coverage and tests", command: "c8 ava" },
+ * ]
+ * ```
+ */
 export const getCommands = (input: string[]) => input.map(task => {
-	// TODO: use regex?
-	const [taskTitle, ...command] = task.split(":");
-	const isNamedTask = command.length > 0;
+	// eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
+	const command = task.match(commandRegex) as RegExpGroups<"title" | "command">;
+	const isNamedTask = command !== null;
 
 	if (isNamedTask) {
-		return { taskTitle, command: command.join(":") };
+		return { taskTitle: command.groups.title, command: command.groups.command };
 	}
 
 	const [commandName] = task.split(" ", 1);
--- src/reset.d.ts
+++ src/reset.d.ts
@@ -1,22 +1,24 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+
 // From https://github.com/total-typescript/ts-reset/pull/110
 
 type Split<Splitter extends string, Limit extends number> = (
 	Limit extends 0
-		? [] // eslint-disable-line @typescript-eslint/ban-types
+		? []
 		: Splitter extends ""
 			? string[]
 			: [string, ...string[]]
 );
 
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface String {
 	split<Splitter extends string, Limit extends number>(
 		splitter: { [Symbol.split](string: Splitter, limit?: number): Split<Splitter, Limit> },
 		limit?: Limit
 	): Split<Splitter, Limit>;
+
 	split<Seperator extends string, Limit extends number>(
 		seperator: Seperator,
 		limit?: Limit
 	): Split<Seperator, Limit>;
 }
-
